75fed681a4a912654088f7c743e2d7007b9204d7
Hashem Abdou <98241106+MHA963@users.noreply.github.com>
2024-02-25 19:56:17 +0100

    repo init + 1.2

---
+++ ConcurrencyLab1/ConcurrencyLab1/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+
+namespace ConcurrencyLab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ConcurrencyLab1 started.");
+
+            SeedDb.SeedVaccines();
+
+            // Start a new thread to simulate concurrent update
+            Thread thread = new Thread(ConcurrentUpdate);
+            thread.Start();
+
+            using (var db = new AppDbContext())
+            {
+                var record = db.VaccineShots.First();
+
+                var count = record.FirstShot;
+                Console.WriteLine("Count read: " + count);
+
+                // Simulate local update
+                count++;
+                record.FirstShot = count;
+
+                // Write to database
+                db.SaveChanges();
+            }
+
+            Console.WriteLine("Main thread finished.");
+        }
+
+        static void ConcurrentUpdate()
+        {
+            using (var db = new AppDbContext())
+            {
+                // Simulate update on another thread
+                var record = db.VaccineShots.First();
+                var count = record.FirstShot;
+                Console.WriteLine("Concurrent thread read: " + count);
+                Thread.Sleep(10000);
+                // Increment the count and save changes
+                count++;
+                db.Database.ExecuteSqlRaw(
+                    "UPDATE dbo.VaccineShots SET FirstShot = @p0 WHERE VaccineShotsId = 1",
+                    count);
+
+                Console.WriteLine("Concurrent thread finished.");
+            }
+        }
+    }
+}
---
+++ ConcurrencyLab1/ConcurrencyLab1/VaccineShots.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrencyLab1
+{
+    public class VaccineShots
+    {
+        public int VaccineShotsId { get; set; }
+        public int FirstShot { get; set; }
+        public int SecondShot { get; set; }
+        public int ThirdShot { get; set; }
+        public int FourthShot { get; set; }
+    }
+}
